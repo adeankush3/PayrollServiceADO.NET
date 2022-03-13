f05cb1dd14764dd4069416b513690e915cf8698b
adeankush3 <adeankush3@gmail.com>
2022-03-13 21:59:13 +0530

    [Ankush]PayRoll DB ANd SQL Connection

---
+++ EmployeePayRollService/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayRollService
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Payroll Service program using MSSQL & ADO.NET \n");
+            EmployeeOperations eops = new EmployeeOperations();
+            eops.GetEmployeeDetails();
+        }
+    }
+}
